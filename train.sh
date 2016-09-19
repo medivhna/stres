33992da43460f567ba3d22ac7fdbe7c4aa348cf0
Medivhna <teljerry@live.cn>
2016-09-19 14:25:30 +0800

    Test1_2

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
-CUDA_VISIBLE_DEVICES=0,1 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python python/resnet_train.py \
 --batch_size=128 \
---num_gpus=2 \
+--num_gpus=4 \
 --lr_decay_epoches=4.0 \
---num_examples=4598268 \
---num_classes=93643 \
---train_list_path='/home/wangguanshuo/lists/MSRA/train.npy' \
---train_dir='/home/wangguanshuo/tf/model/resnet_50' \
---summary_dir='/home/wangguanshuo/tf/summary/resnet_50' \
+--num_examples=4600023 \
+--num_classes=93644 \
+--train_list_path='/home/wangguanshuo/lists/cropped/stn_cropped.npy' \
+--train_dir='/home/wangguanshuo/tf/model/stres' \
+--summary_dir='/home/wangguanshuo/tf/summary/stres' \
 
 # For more param changes in resnet_train.py
