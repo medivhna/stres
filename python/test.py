4e6a5eee1706dcb7e688dc911f757ddcd12de74c
Medivhna <teljerry@live.cn>
2016-09-26 17:41:43 +0800

    Test_final

--- python/test.py
+++ python/test.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 
 def main():
-    line_queue = tf.train.string_input_producer([self.list_path])
+    train = True
+    line_queue = tf.train.string_input_producer(['/home/wangguanshuo/lists/cropped/stn_cropped_with_params.npy'])
     reader = tf.TextLineReader()
     _, value = reader.read(line_queue)
     record_defaults = [[""], [0], [0], [0], [0], [0],
@@ -10,22 +11,21 @@ def main():
     filenames, labels, x, h, y, w, t1, t2, t3, t4, t5, t6 = tf.decode_csv(value, 
                                                                           record_defaults=record_defaults,
                                                                           field_delim=' ')    
-
-    for tid in range(self.num_preprocess_threads):
-        image = tf.image.decode_jpeg(tf.read_file(filenames), channels=self.depth)
+    batches = []
+    for tid in range(4):
+        image = tf.image.decode_jpeg(tf.read_file(filenames), channels=3)
         image = tf.image.convert_image_dtype(image, dtype=tf.float32)
         image = tf.image.crop_to_bounding_box(image, x, y, h, w)
-        tsf_params = tf.pack([t1, t2, t3, t4, t5, t6])
+        tsf_params = [t1, t2, t3, t4, t5, t6]
         
-        if train:
-            image = self.distort_image(image, self.height, self.width, bbox=[], thread_id=tid)
-            batches.append([image, labels, tsf_params])
-        else:
-            image = self.eval_image(image, self.height, self.width)
-            batches.append([image, labels])
+        batches.append([image, labels, tsf_params])
     
-    print(batches)
-   
+    images, label_index_batch, transforms = tf.train.shuffle_batch_join(
+            batches,
+            batch_size=128,
+            capacity=2 * 4 * 128,
+            min_after_dequeue = 2 * 128) 
+    print(images, label_index_batch, transforms)
     # with tf.Session() as sess:
         # # Start populating the filename queue.
         # coord = tf.train.Coordinator()
