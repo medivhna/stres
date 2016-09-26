fb17f03d52f1ab7f6c3514b04061239b185d6e0f
Medivhna <teljerry@live.cn>
2016-09-26 17:28:09 +0800

    Test change 2

--- python/datasets.py
+++ python/datasets.py
@@ -217,10 +217,25 @@ class DataSet:
         line_queue = tf.train.string_input_producer([self.list_path])
         reader = tf.TextLineReader()
         _, value = reader.read(line_queue)
-        
         record_defaults = [[""], [0], [0], [0], [0], [0],
                            [.1], [.1],[.1],[.1],[.1],[.1]]
-        filenames, labels, x, h, y, w, t1, t2, t3, t4, t5, t6 = 
-                          tf.decode_csv(value, record_defaults=record_defaults)
+        filenames, labels, x, h, y, w, t1, t2, t3, t4, t5, t6 = tf.decode_csv(value, 
+                                                                              record_defaults=record_defaults,
+                                                                              field_delim=' ')    
+    
+        for tid in range(self.num_preprocess_threads):
+            image = tf.image.decode_jpeg(tf.read_file(filenames), channels=self.depth)
+            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+            image = tf.image.crop_to_bounding_box(image, x, y, h, w)
+            tsf_params = tf.pack([t1, t2, t3, t4, t5, t6])
+            
+            if train:
+                image = self.distort_image(image, self.height, self.width, bbox=[], thread_id=tid)
+                batches.append([image, labels, tsf_params])
+            else:
+                image = self.eval_image(image, self.height, self.width)
+                batches.append([image, labels])
+        
+        print(batches)
         
         
--- python/test.py
+++ python/test.py
@@ -2,29 +2,41 @@ import tensorflow as tf
 import numpy as np
 
 def main():
-    line_queue = tf.train.string_input_producer(['/home/wangguanshuo/lists/cropped/stn_cropped_with_params.npy'])
+    line_queue = tf.train.string_input_producer([self.list_path])
     reader = tf.TextLineReader()
     _, value = reader.read(line_queue)
-    
     record_defaults = [[""], [0], [0], [0], [0], [0],
                        [.1], [.1],[.1],[.1],[.1],[.1]]
-    filenames, labels, xs, hs, ys, ws, t1, t2, t3, t4, t5, t6 = tf.decode_csv(value, 
+    filenames, labels, x, h, y, w, t1, t2, t3, t4, t5, t6 = tf.decode_csv(value, 
                                                                           record_defaults=record_defaults,
-                                                                          field_delim=' ')
-    image = tf.image.decode_jpeg(tf.read_file(filenames)) 
-   
-    with tf.Session() as sess:
-        # Start populating the filename queue.
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
+                                                                          field_delim=' ')    
 
+    for tid in range(self.num_preprocess_threads):
+        image = tf.image.decode_jpeg(tf.read_file(filenames), channels=self.depth)
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = tf.image.crop_to_bounding_box(image, x, y, h, w)
+        tsf_params = tf.pack([t1, t2, t3, t4, t5, t6])
         
-        # Retrieve a single instance:
-        x, y, h, w = sess.run([xs, ys, hs, ws])
-        print(x, y, h, w)
+        if train:
+            image = self.distort_image(image, self.height, self.width, bbox=[], thread_id=tid)
+            batches.append([image, labels, tsf_params])
+        else:
+            image = self.eval_image(image, self.height, self.width)
+            batches.append([image, labels])
+    
+    print(batches)
+   
+    # with tf.Session() as sess:
+        # # Start populating the filename queue.
+        # coord = tf.train.Coordinator()
+        # threads = tf.train.start_queue_runners(coord=coord)
+        
+        # # Retrieve a single instance:
+        # x, y, h, w = sess.run([xs, ys, hs, ws])
+        # print(x, y, h, w)
 
-        coord.request_stop()
-        coord.join(threads)
+        # coord.request_stop()
+        # coord.join(threads)
 
 if __name__ == '__main__':
     main()
